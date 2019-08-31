540fccd1a2b89b3946c0b2e3a8407432d8d5389d
cristian david tilano silva <cristiantilanosilva@gmail.com>
2019-08-30 19:14:11 -0500

    punto 4 del taller, promedio notas

---
+++ taller4/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace taller4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblnota1 = new System.Windows.Forms.Label();
+            this.lblnota2 = new System.Windows.Forms.Label();
+            this.lblnota3 = new System.Windows.Forms.Label();
+            this.txtnota1 = new System.Windows.Forms.TextBox();
+            this.txtnota2 = new System.Windows.Forms.TextBox();
+            this.txtnota3 = new System.Windows.Forms.TextBox();
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(217, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculador de promedios";
+            // 
+            // lblnota1
+            // 
+            this.lblnota1.AutoSize = true;
+            this.lblnota1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnota1.Location = new System.Drawing.Point(65, 133);
+            this.lblnota1.Name = "lblnota1";
+            this.lblnota1.Size = new System.Drawing.Size(55, 19);
+            this.lblnota1.TabIndex = 1;
+            this.lblnota1.Text = "Nota 1:";
+            // 
+            // lblnota2
+            // 
+            this.lblnota2.AutoSize = true;
+            this.lblnota2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnota2.Location = new System.Drawing.Point(65, 197);
+            this.lblnota2.Name = "lblnota2";
+            this.lblnota2.Size = new System.Drawing.Size(55, 19);
+            this.lblnota2.TabIndex = 2;
+            this.lblnota2.Text = "Nota 2:";
+            // 
+            // lblnota3
+            // 
+            this.lblnota3.AutoSize = true;
+            this.lblnota3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnota3.Location = new System.Drawing.Point(65, 261);
+            this.lblnota3.Name = "lblnota3";
+            this.lblnota3.Size = new System.Drawing.Size(55, 19);
+            this.lblnota3.TabIndex = 4;
+            this.lblnota3.Text = "Nota 3:";
+            // 
+            // txtnota1
+            // 
+            this.txtnota1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnota1.Location = new System.Drawing.Point(177, 132);
+            this.txtnota1.Name = "txtnota1";
+            this.txtnota1.Size = new System.Drawing.Size(100, 25);
+            this.txtnota1.TabIndex = 5;
+            // 
+            // txtnota2
+            // 
+            this.txtnota2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnota2.Location = new System.Drawing.Point(177, 198);
+            this.txtnota2.Name = "txtnota2";
+            this.txtnota2.Size = new System.Drawing.Size(100, 25);
+            this.txtnota2.TabIndex = 6;
+            // 
+            // txtnota3
+            // 
+            this.txtnota3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnota3.Location = new System.Drawing.Point(177, 262);
+            this.txtnota3.Name = "txtnota3";
+            this.txtnota3.Size = new System.Drawing.Size(100, 25);
+            this.txtnota3.TabIndex = 7;
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnenviar.Location = new System.Drawing.Point(376, 180);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(105, 52);
+            this.btnenviar.TabIndex = 8;
+            this.btnenviar.Text = "Calcular";
+            this.btnenviar.UseVisualStyleBackColor = true;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 21);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "TOTAL:";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbltotal.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotal.Location = new System.Drawing.Point(167, 366);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(19, 21);
+            this.lbltotal.TabIndex = 10;
+            this.lbltotal.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnenviar);
+            this.Controls.Add(this.txtnota3);
+            this.Controls.Add(this.txtnota2);
+            this.Controls.Add(this.txtnota1);
+            this.Controls.Add(this.lblnota3);
+            this.Controls.Add(this.lblnota2);
+            this.Controls.Add(this.lblnota1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblnota1;
+        private System.Windows.Forms.Label lblnota2;
+        private System.Windows.Forms.Label lblnota3;
+        private System.Windows.Forms.TextBox txtnota1;
+        private System.Windows.Forms.TextBox txtnota2;
+        private System.Windows.Forms.TextBox txtnota3;
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbltotal;
+    }
+}
+
---
+++ taller4/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taller4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviar_Click(object sender, EventArgs e)
+        {
+            double total;
+            total = ((double.Parse(txtnota1.Text) + double.Parse(txtnota2.Text) + double.Parse(txtnota3.Text)) / 3);
+            lbltotal.Text = total.ToString();
+        }
+    }
+}
